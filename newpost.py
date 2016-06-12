c66011ccded6a203ee47f71d7f602cb9e712a105
Rohitt Shinde <shinde.rohitt@gmail.com>
2016-06-12 19:07:16 +0530

    add a pyscript to create new dummy post

---
+++ newpost.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import sys
+from os import path
+from datetime import datetime
+
+HEADER = """
+---
+layout: post
+title: "{title}"
+date: "{now}"
+author: "Rohitt Shinde"
+tags:
+- tag1
+- tag2
+---
+
+
+"""
+
+current_time = datetime.now()
+day = current_time.strftime("%Y-%m-%d")
+hours = current_time.strftime("%H:%M")
+title = " ".join(sys.argv[1:])
+filename = day + "-" + title.replace(' ', '-') + '.markdown'
+
+
+HEADER = HEADER.format(
+    title = title,
+    now = day + ' ' + hours,
+)
+
+target_path = path.join('_posts', filename)
+with open(target_path, 'wt') as file_out:
+    file_out.write(HEADER)
+
+print 'File is at ', target_path
