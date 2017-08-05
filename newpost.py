e4b6c3293c798c5b874cfededc8746bd9a84d3eb
Rohit <shinde.rohitt@gmail.com>
2017-08-05 19:40:57 +0530

    Improve newpost.py

--- newpost.py
+++ newpost.py
@@ -22,18 +22,20 @@ current_time = datetime.now()
 day = current_time.strftime("%Y-%m-%d")
 hours = current_time.strftime("%H:%M")
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     print '''Need title as well :)
-    $./newpost.py New Post Title Here'''
+    $./newpost.py "New Post Title Here"'''
     sys.exit(1)
 
-title = " ".join(sys.argv[1:])
-filename = day + "-" + title.replace(' ', '-') + '.markdown'
+title = sys.argv[1]
+sane_title = title.translate(None, "?.!/;:,")
+
+filename = day + "-" + sane_title.replace(' ', '-') + '.markdown'
 
 
 HEADER = HEADER.format(
-    title = title,
-    now = day + ' ' + hours,
+    title=title,
+    now=day + ' ' + hours,
 )
 
 target_path = path.join('_posts', filename)
